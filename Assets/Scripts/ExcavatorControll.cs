37265393de360bfd06ae523c85e7245d45c66256
mumbosoft <Patrick_k@live.se>
2017-10-17 22:25:19 +0200

    Terrain, controll etc. for Excavator

--- Assets/Scripts/ExcavatorControll.cs
+++ Assets/Scripts/ExcavatorControll.cs
@@ -4,13 +4,92 @@ using UnityEngine;
 
 public class ExcavatorControll : MonoBehaviour {
 
+    private ExcavatorController exController;
+    private bool driveMode;
 	// Use this for initialization
 	void Start () {
-		
+        exController = new ExcavatorController();
+        driveMode = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (driveMode)
+            {
+                driveMode = false;
+            }
+            else
+            {
+                driveMode = true;
+            }
+            Debug.Log(driveMode);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            exController.rotateArm1(-10f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            exController.rotateArm1(10f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            exController.rotateBase(10f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            exController.rotateBase(-10f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (driveMode)
+            {
+                exController.drive(5f);
+            }
+            else
+            {
+                exController.rotateArm2(-10f);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (driveMode)
+            {
+                exController.drive(-5f);
+            }
+            else
+            {
+                exController.rotateArm2(10f);
+            }
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (driveMode)
+            {
+                exController.rotateDrive(10f);
+            }
+            else
+            {
+                exController.rotateBucket(-10f);
+            }
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (driveMode)
+            {
+                exController.rotateDrive(-10f);
+            }
+            else
+            {
+                exController.rotateBucket(10f);
+
+
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/ExcavatorController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcavatorController {
+
+	private GameObject Base, Arm1, Arm2, Bucket, Machine;
+    private Rigidbody BaseBody, Arm1Body, Arm2Body, BucketBody;
+    private bool drivemode;
+	public ExcavatorController(){
+		Base = GameObject.Find ("Base1");
+        BaseBody = Base.GetComponent<Rigidbody>();
+		Arm1 = GameObject.Find ("Arm1Joint");
+        Arm1Body = Arm1.GetComponent<Rigidbody>();
+        Arm2 = GameObject.Find ("Arm2Joint");
+        Arm2Body = Arm2.GetComponent<Rigidbody>();
+        Bucket = GameObject.Find ("BucketJoint1");
+        BucketBody = Bucket.GetComponent<Rigidbody>();
+        Machine = GameObject.Find("ExcavatorV2Simple");
+        drivemode = false;
+
+    }
+
+    public void rotateArm1(float upOrDown) {
+        Arm1.transform.Rotate(Vector3.forward * Time.deltaTime * upOrDown);
+    }
+    public void rotateArm2(float upOrDown)
+    {
+        Arm2.transform.Rotate(Vector3.forward * Time.deltaTime * upOrDown);
+    }
+    public void rotateBucket(float upOrDown)
+    {
+        Bucket.transform.Rotate(Vector3.forward * Time.deltaTime * upOrDown);
+    }
+    public void rotateBase(float leftOrRight)
+    {
+        Base.transform.Rotate(Vector3.back * Time.deltaTime * leftOrRight);
+    }
+    public void rotateDrive(float leftOrRight)
+    {
+        Machine.transform.Rotate(Vector3.back * Time.deltaTime * leftOrRight);
+    }
+    public void drive(float driveForOrBac)
+    {
+        Machine.transform.Translate(Vector3.right * Time.deltaTime * driveForOrBac);
+    }
+
+    
+}
