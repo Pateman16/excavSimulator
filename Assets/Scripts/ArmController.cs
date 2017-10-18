01b5a42e09df932210156cf844626931015806b7
HH\patkar15 <patkar15@student.hh.se>
2017-10-18 17:29:14 +0200

    Added alot of functionality including VR

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour {
+
+    public GameObject ArmA, ArmB, BucketMain, Base1;
+    private HingeJoint ArmAHinge, ArmBHinge, BucketHinge, Base1Hinge;
+    private float rotateArmA, rotateArmB, rotateBucketMain, rotateBase1;
+    private bool driveMode;
+    public float rotationSpeed;
+    private ExcavatorController exController;
+
+    // Use this for initialization
+    void Start () {
+        exController = new ExcavatorController();
+        ArmAHinge = ArmA.GetComponent<HingeJoint>();
+        ArmBHinge = ArmB.GetComponent<HingeJoint>();
+        BucketHinge = BucketMain.GetComponent<HingeJoint>();
+        Base1Hinge = Base1.GetComponent<HingeJoint>();
+        rotateArmA = 0f;
+        rotateArmB = 0f;
+        rotateBucketMain = 0f;
+        rotateBase1 = 0f;
+        rotationSpeed = 0.5f;
+        driveMode = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /**
+         Press the tab button to switch from drivingmode to working mode*/
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (driveMode)
+            {
+                driveMode = false;
+                Debug.Log(driveMode);
+            }
+            else
+            {
+                driveMode = true;
+                Debug.Log(driveMode);
+            }
+            
+        }
+
+        /**
+         Press uparrow to controll main arm up*/
+        if (Input.GetKey(KeyCode.UpArrow) && rotateArmA > ArmAHinge.limits.min)
+        {
+            rotateArmA = rotateArmA - rotationSpeed;
+ 
+        }
+        /**
+         press downarrow to controll main arm down*/
+        if (Input.GetKey(KeyCode.DownArrow) && rotateArmA < ArmAHinge.limits.max)
+        {
+            rotateArmA = rotateArmA + rotationSpeed;
+        }
+
+        
+
+        /** This jointspring is ArmA's jointspring which is set.*/
+        JointSpring ArmASpring = ArmAHinge.spring;
+        
+        ArmASpring.targetPosition = rotateArmA;
+        Debug.Log(ArmASpring.targetPosition);
+        ArmAHinge.spring = ArmASpring;  
+
+        /**
+         Press leftarrow to controll the baserotation to the left*/
+        if (Input.GetKey(KeyCode.LeftArrow) && rotateBase1 > Base1Hinge.limits.min)
+        {
+            rotateBase1 = rotateBase1 - rotationSpeed;
+        }
+        /**press the rightarrow to controll the baserotation to the right*/
+        if (Input.GetKey(KeyCode.RightArrow) && rotateBase1 < Base1Hinge.limits.max)
+        {
+            rotateBase1 = rotateBase1 + rotationSpeed;
+        }
+        /** This jointspring is Base1's jointspring which is set.*/
+        JointSpring Base1Spring = Base1Hinge.spring;
+
+        Base1Spring.targetPosition = rotateBase1;
+        Debug.Log(Base1Spring.targetPosition);
+        Base1Hinge.spring = Base1Spring;
+        /**
+         If drivemode = true this is the acceleraton else its the controll of the second arm*/
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (driveMode)
+            {
+                exController.drive(15000000f, Time.deltaTime);
+            }
+            else if(rotateArmB > ArmBHinge.limits.min)
+            {
+                rotateArmB = rotateArmB - rotationSpeed;
+            }
+        }
+        /*If drivemode = true this is the deceleraton else its the controll of the second arm 
+         * **/
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (driveMode)
+            {
+                exController.drive(-15000000f, Time.deltaTime);
+            }
+            else if (rotateArmB < ArmBHinge.limits.max)
+            {
+                rotateArmB = rotateArmB + rotationSpeed;
+            }
+
+        }
+
+        /** This jointspring is ArmB's jointspring which is set.*/
+        JointSpring ArmBSpring = ArmBHinge.spring;
+
+        ArmBSpring.targetPosition = rotateArmB;
+        Debug.Log(ArmBSpring.targetPosition);
+        ArmBHinge.spring = ArmBSpring;
+
+        /**
+         If drivemode is enabled this is the turn of the vehicle, else its the bucket rotation.*/
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (driveMode)
+            {
+                exController.rotateDrive(15f);
+            }
+            else if (rotateBucketMain > BucketHinge.limits.min)
+            {
+                rotateBucketMain = rotateBucketMain - rotationSpeed;
+            }
+
+        }
+        /**
+         If drivemode is enabled this is the turn of the vehicle, else its the bucket rotation.*/
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (driveMode)
+            {
+                exController.rotateDrive(-15f);
+            }
+            else if (rotateBucketMain < BucketHinge.limits.max)
+            {
+                rotateBucketMain = rotateBucketMain + rotationSpeed;
+            }
+        }
+        /** This jointspring is Base1's jointspring which is set.*/
+        JointSpring BucketMainSpring = BucketHinge.spring;
+
+        BucketMainSpring.targetPosition = rotateBucketMain;
+        Debug.Log(BucketMainSpring.targetPosition);
+        BucketHinge.spring = BucketMainSpring;
+    }
+}
--- Assets/Scripts/ExcavatorController.cs
+++ Assets/Scripts/ExcavatorController.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 public class ExcavatorController {
 
 	private GameObject Base, Arm1, Arm2, Bucket, Machine;
-    private Rigidbody BaseBody, Arm1Body, Arm2Body, BucketBody;
-    private bool drivemode;
+    private Rigidbody BaseBody, Arm1Body, Arm2Body, BucketBody, MachineBody;
+    private JointMotor arm1Motor, arm2Motor, bucketMotor;
+    private float maxSpeed;
+
 	public ExcavatorController(){
 		Base = GameObject.Find ("Base1");
         BaseBody = Base.GetComponent<Rigidbody>();
-		Arm1 = GameObject.Find ("Arm1Joint");
+		Arm1 = GameObject.Find ("ArmA");
         Arm1Body = Arm1.GetComponent<Rigidbody>();
         Arm2 = GameObject.Find ("Arm2Joint");
         Arm2Body = Arm2.GetComponent<Rigidbody>();
         Bucket = GameObject.Find ("BucketJoint1");
         BucketBody = Bucket.GetComponent<Rigidbody>();
         Machine = GameObject.Find("ExcavatorV2Simple");
-        drivemode = false;
+        MachineBody = Machine.GetComponent<Rigidbody>();
+        maxSpeed = 30f;
+
+        arm1Motor = Arm1.GetComponent<HingeJoint>().motor;
+        arm1Motor.targetVelocity = 1;
+        arm1Motor.freeSpin = false;
 
     }
 
     public void rotateArm1(float upOrDown) {
-        Arm1.transform.Rotate(Vector3.forward * Time.deltaTime * upOrDown);
+        //Arm1.transform.Rotate(Vector3.forward * Time.deltaTime * upOrDown);
+        arm1Motor.force = upOrDown;
     }
     public void rotateArm2(float upOrDown)
     {
-        Arm2.transform.Rotate(Vector3.forward * Time.deltaTime * upOrDown);
+        //Arm2.transform.Rotate(Vector3.forward * Time.deltaTime * upOrDown);
     }
     public void rotateBucket(float upOrDown)
     {
@@ -40,9 +48,14 @@ public class ExcavatorController {
     {
         Machine.transform.Rotate(Vector3.back * Time.deltaTime * leftOrRight);
     }
-    public void drive(float driveForOrBac)
+    public void drive(float driveForOrBac, float Time)
     {
-        Machine.transform.Translate(Vector3.right * Time.deltaTime * driveForOrBac);
+        if (MachineBody.velocity.sqrMagnitude < maxSpeed)
+        {
+            MachineBody.AddRelativeForce(Vector3.right * driveForOrBac * Time / MachineBody.mass, ForceMode.Acceleration); //Car.transform.forward * Forward * Time.deltaTime / rigidbody.mass, ForceMode.Acceleration
+        }
+        Debug.Log(MachineBody.velocity.sqrMagnitude);
+        //Machine.transform.Translate(Vector3.right * Time.deltaTime * driveForOrBac);
     }
 
     
